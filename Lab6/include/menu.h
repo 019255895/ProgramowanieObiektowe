af5ef8240f2057dea34f7a9beec9438f71a077c6
019255895 <62744378+019255895@users.noreply.github.com>
2020-06-10 13:41:27 +0200

    Update menu.h

--- Lab6/include/menu.h
+++ Lab6/include/menu.h
@@ -15,4 +15,36 @@ namespace UserMenu
      * @brief run menu write process
      */
     void run();
+    
+    
+    // struct Component
+    // {
+    //     const char * selectionText;
+    //     virtual void execute() = 0;
+    // };
+
+    // struct FirtSelection : public Component
+    // {
+    //     const char * selectionText = "First selection";
+    //     void execute() {
+    //         std::cout << "first";
+    //     }
+    // };
+
+    // class Menu
+    // {
+    // private:
+    //     std::vector<Component*> components;
+    // public:
+    //     void add(Component* c){
+    //         components.push_back(c);
+    //     }
+
+    //     void run(){
+    //         std::vector<Component*> ::iterator it;
+    //         for(it = components.begin(); it < components.end(); it++){
+    //             (*it)->execute();
+    //         }
+    //     }
+    // };
 } // namespace UserMenu
