394ddea90fbd389a144cbd4625c7fa8fe80abeba
019255895 <62744378+019255895@users.noreply.github.com>
2020-06-10 13:41:04 +0200

    Update table.h

--- Lab6/include/table.h
+++ Lab6/include/table.h
@@ -20,36 +20,5 @@ namespace UserTable
         /// Set string value on cell
         void setStringValue(std::string value, int xPosition, int yPosition);
     };   
-
-    // struct Component
-    // {
-    //     const char * selectionText;
-    //     virtual void execute() = 0;
-    // };
-
-    // struct FirtSelection : public Component
-    // {
-    //     const char * selectionText = "First selection";
-    //     void execute() {
-    //         std::cout << "first";
-    //     }
-    // };
-
-    // class Menu
-    // {
-    // private:
-    //     std::vector<Component*> components;
-    // public:
-    //     void add(Component* c){
-    //         components.push_back(c);
-    //     }
-
-    //     void run(){
-    //         std::vector<Component*> ::iterator it;
-    //         for(it = components.begin(); it < components.end(); it++){
-    //             (*it)->execute();
-    //         }
-    //     }
-    // };
 } // namespace UserTable
 
